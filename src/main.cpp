52346683a16d6c0ac4bb1ad708f3248427f300d5
VadikLowKick <vadimsav70@gmail.com>
2024-05-29 19:45:24 +0700

    Perparing final version of project again

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,16 @@
 #include <QtQuick>
 #include "class1.h"
 #include "filereader.h"
-#include "filecounter.h"
 
 int main(int argc, char *argv[])
 {
     QScopedPointer<QGuiApplication> application(Aurora::Application::application(argc, argv));
     application->setOrganizationName(QStringLiteral("ru.auroraos"));
     application->setApplicationName(QStringLiteral("eBooksReader"));
-
-    qmlRegisterType<Class1>("ru.omp.SimpleBLE", 1, 0, "Class1");
     qmlRegisterType<FileReader>("ru.omp.FileReader", 1, 0, "FileReader");
 
     QScopedPointer<QQuickView> view(Aurora::Application::createView());
 
-    // Создаем объект вашего QML класса
-    Class1 class1Object;
 
     // Создаем контекст приложения и устанавливаем в него объект через setProperty()
     QQmlContext *context = view->rootContext();
